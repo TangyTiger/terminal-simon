0d5fb8ff961eeffab00c91ebc6a1b4fb3bd39c0e
Aditya Shah <59452496+TangyTiger@users.noreply.github.com>
2021-12-17 13:01:06 -0800

    added commits cleaned up functions

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ score = 0
   #get_input(pattern)
   #check_pattern(user_input, pattern)
 
+#creating pattern with "colors"
 def create_pattern():
   pattern.append(random.choice(colors))
   #print(pattern)
   #return pattern
   print_pattern(pattern)
 
+  
+#displaying pattern, with clearing the outputs before
 def print_pattern(pattern):
   for i in range(len(pattern)):
     print(pattern[i])
@@ -28,7 +31,9 @@ def print_pattern(pattern):
     #clear()
   get_input(pattern)
 
-def get_input(pattern,):
+  
+#getting input
+def get_input(pattern):
   string_user_input = input("What is the pattern: ").upper()
   user_input = list(string_user_input)
   #print(user_input)
@@ -36,7 +41,7 @@ def get_input(pattern,):
     print("Sorry better luck next time the pattern was " + "".join(pattern))
     game_over()
   else:
-    check_pattern(user_input, pattern, )
+    check_pattern(user_input, pattern)
 
   for i in range(len(user_input)):
     if  user_input[i] != "A" and user_input[i] != "B" and user_input[i] != "C" and user_input[i] != "D":
@@ -44,7 +49,8 @@ def get_input(pattern,):
       get_input()
   return user_input
 
-def check_pattern(user_input, pattern, ):
+#check user patten
+def check_pattern(user_input, pattern):
   for i in range(len(user_input)):
       if user_input[i] != pattern[i]:
         print("Sorry better luck next time, the pattern was", "".join(pattern))
@@ -53,12 +59,14 @@ def check_pattern(user_input, pattern, ):
       else:
         create_pattern()
 
+#clear terminal to hide what was outputed before        
 def clear():
   print("In clear")
   if name == 'nt': 
     _ = system('cls') 
 
 
+#start new game
 def game_over():
   print("Press RUN to play again")
 
